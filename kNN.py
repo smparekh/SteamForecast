8df5f40a2b12b96a6420073f5c8eeb28cab2af86
Shaishav Parekh <parekhsm@vt.edu>
2013-11-24 23:03:29 -0500

    Added kNN.py, k-Nearest not yet implemented. Parsing of dataset is semi-successful. Suggest including current year in first line of dataset to properly initialize matrix.

---
+++ kNN.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python
+import sys, getopt, string
+import numpy as np
+
+def main(argv):
+    inputfile = ''
+
+    try:
+        opts, args = getopt.getopt(argv, 'hi:')
+    except getopt.GetoptError:
+        print 'Usage: kNN.py -i <inputfile>'
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print 'Usage: kNN.py -i <inputfile>'
+            sys.exit()
+        elif opt in ("-i", "--ifile"):
+            inputfile = arg
+    try:
+        infile = open(inputfile, 'r')
+    except IOError:
+        print 'File not found, check name and existence.'
+        sys.exit(2)
+    print 'Parsing file: ', inputfile
+
+    rlsLine = infile.readline()
+    rlsLine = rlsLine.strip()
+    rlsLine = rlsLine.split(", ")
+    rlsDate = int(rlsLine[0])
+    rlsYear = int(rlsLine[1])
+    print rlsDate, ", ", rlsYear
+
+    inDates = []
+    inYears = []
+    numRows = 2 #max(inYears)
+    numCols = 365
+    saleDates = np.zeros((numRows, numCols), dtype=np.int)
+
+    for line in infile.readlines():
+        #print line
+        line = line.strip()
+        inDateYear = line.split(", ")
+        #print inDateYear[0]
+        #print inDateYear[1]
+        inDates.append(int(inDateYear[0]))
+        inYears.append(int(inDateYear[1]))
+        saleDates[int(inDateYear[1])-1][int(inDateYear[0])-1] = 1;
+    saleDates[int(inDateYear[1])-1][int(inDateYear[0])-1:] = -1; 
+    #print len(inDates)
+    #print len(inYears)
+    
+    saleDates[rlsYear-1][0:rlsDate] = -1
+    print saleDates
+
+if __name__ == "__main__": 
+    main(sys.argv[1:])
+
