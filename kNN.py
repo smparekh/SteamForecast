62f5ce9f8e3f1bdd1426caba99d44b82b349cde5
Shaishav Parekh <parekhsm@vt.edu>
2013-11-27 18:39:45 -0500

    kNN.py now needs another argument 'k' which is the k in kNN. Program prints input file name, current day, sale range matrix, number of sale days and non-sale days around current day based on k.

--- kNN.py
+++ kNN.py
@@ -5,18 +5,20 @@ from datetime import datetime
 
 def main(argv):
     inputfile = ''
-
+    kstr = ''
     try:
-        opts, args = getopt.getopt(argv, 'hi:')
+        opts, args = getopt.getopt(argv, 'hi:k:')
     except getopt.GetoptError:
-        print 'Usage: kNN.py -i <inputfile>'
+        print 'Usage: kNN.py -i <inputfile> -k <k for knn>'
         sys.exit(2)
     for opt, arg in opts:
         if opt == '-h':
-            print 'Usage: kNN.py -i <inputfile>'
+            print 'Usage: kNN.py -i <inputfile> -k <k for knn>'
             sys.exit()
         elif opt in ("-i", "--ifile"):
             inputfile = arg
+        elif opt in ("-k"):
+            kstr = arg
     try:
         infile = open(inputfile, 'r')
     except IOError:
@@ -30,7 +32,7 @@ def main(argv):
     rlsDate = int(rlsLine[0])
     rlsYear = int(rlsLine[1])
     curYear = int(rlsLine[2])
-    print rlsDate, ", ", rlsYear, ", ", curYear
+    #print rlsDate, ", ", rlsYear, ", ", curYear
 
     inDates = []
     inYears = []
@@ -53,7 +55,13 @@ def main(argv):
     #print len(inYears)
     
     saleDates[rlsYear-1][0:rlsDate] = -1
-    print saleDates
+    k = int(kstr)
+    #print saleDates
+    saleRange = saleDates[:,curDay-k:curDay+k];
+    print 'Current Day: ',curDay;
+    print saleRange
+    print (saleRange==1).sum(), (saleRange==0).sum()
+    infile.close()
 
 if __name__ == "__main__": 
     main(sys.argv[1:])
